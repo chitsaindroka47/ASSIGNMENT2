c55dc3813d0fcd008ac18d2b5251c2adb75cd405
Chitranjan Singh <chitranjan.22jics211@jietjodhpur.ac.in>
2025-06-01 21:12:50 +0530

    Final Changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ import TodoList from "./components/TodoList";
 export default function App() {
   return (
     <div className="container">
-      <h1>📝 React To-Do List</h1>
+      <h1>📝To-Do List</h1>
       <TodoList />
+       <footer className="footer">
+        <hr />
+        <p>Designed with ❤️ by Chitranjan Singh</p>
+      </footer>
     </div>
   );
 }
